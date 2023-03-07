4f5a4f0458e4ffa485084f0192bbcf1865017cb0
Metin Oktay <mettinoktay@gmail.com>
2023-03-07 17:26:11 +0100

    clean directory

--- compeng/tobenamed.py
+++
@@ -1,6 +0,0 @@
-from sense_hat import SenseHat
-
-sense = SenseHat()
-
-sense.show_message("Humidityis: %" + str(round(sense.get_humidity(), 2)))
-sense.show_message("The attitude is: " + str(round(sense.get_gyro_x(), 2)))
