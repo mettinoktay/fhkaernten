f1ac354599d2c04dee3ec713037e3d311fce33a3
Metin Oktay <mettinoktay@gmail.com>
2023-09-23 11:06:05 +0200

    final

---
+++ AppStat/final/Cornerstone/_distribute.bat
@@ -0,0 +1,15 @@
+REM @echo off
+set component_name=CornerstoneE
+set s_package=CEDAE
+set pck_type=+PUR
+set ILOG=C:\TEMP\_DISTR_CS.LOG
+set distr_to_DCVI=N
+set distr_to_VI=N
+set distr_to_GDR=Y
+set distr_to_SERVER=N
+set admin_dir=..\..\..\..\MacT\+VSA_ADM
+call %admin_dir%\+AT\+DISTR\Distribute_ALL_rel.bat
+
+
+
+
--- AppStat/final/main.py
+++ AppStat/final/main.py
@@ -1,25 +1,20 @@
 import matplotlib.pyplot as plt
 
-first = [218112, 0, 0, -208536, 
-             431818, 199766, 234546, 0, 
-             146907, -371739, -339285, -822115, 
-             423267, -172379, -131944, -777272]
-second = [223821, 0, 0, -142500,
-              415048, 218112, 188325, 0,
-              165057, -347560, -356250, -890625,
-              328846, -175204, -145408, -737068]
+first_run = [218112, 0, 0, -208536, 431818, 199766, 234546, 0, 146907, -371739, -339285, -822115, 423267, -172379,
+             -131944, -777272]
+second_run = [223821, 0, 0, -142500, 415048, 218112, 188325, 0, 165057, -347560, -356250, -890625, 328846, -175204,
+              -145408, -737068]
 
-diff = []
+difference = []
 
 for index in range(0, 16):
     try:
-        diff.append(
-            100 * (second[index] - first[index]) / second[index])
+        difference.append(100 * (second_run[index] - first_run[index]) / second_run[index])
     except ZeroDivisionError:
-        diff.append(0)
+        difference.append(0)
 
 
-plt.plot(diff, linestyle='--', marker='o', color='b')
+plt.plot(difference, linestyle='--', marker='o', color='b')
 plt.ylim([-50, 50])
 plt.xlabel('Run Number', fontsize=16)
 plt.xticks(fontsize = 16)
